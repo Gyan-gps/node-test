e86b9b3431048685d268ab07fb94887ac233e41b
Gyan Prakash Singh <gyangps1998@gmail.com>
2022-12-31 11:39:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,569 @@
+const express = require("express");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+const UserSchema = require("./UserSchema");
+const session = require("express-session");
+const mongoDBSession = require("connect-mongodb-session")(session);
+const jwt = require("jsonwebtoken");
+require('dotenv').config()
+//models
+const TodoModel = require("./models/TodoModel");
+
+//middlewares
+const {
+  cleanUpAndValidate,
+  jwtSign,
+  sendVerifcationEmail,
+} = require("./utils/AuthUtils");
+const isAuth = require("./middleware/isAuth");
+const rateLimitng = require("./middleware/rateLimiting");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+//connection with
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.mongoDB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((res) => {
+    console.log("Connect to DB successfully");
+  })
+  .catch((err) => {
+    console.log("Failed to connect", err);
+  });
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+//Adding the session
+
+const store = new mongoDBSession({
+  uri: process.env.mongoDB_URL,
+  collection: "sessions",
+});
+
+app.use(
+  session({
+    secret: "hello backendjs",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+
+//routes
+app.get("/", (req, res) => {
+  res.send("Welcome to my app");
+});
+
+app.get("/login", (req, res) => {
+  return res.render("login");
+});
+
+app.get("/registration", (req, res) => {
+  return res.render("register");
+});
+
+
+
+app.post("/register", async (req, res) => {
+  console.log(req.body);
+  const { name, email, username, password } = req.body;
+  try {
+    await cleanUpAndValidate({ name, password, email, username });
+  } catch (err) {
+    return res.send({
+      status: 400,
+      message: err,
+    });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 7);
+
+  let user = new UserSchema({
+    name: name,
+    username: username,
+    password: hashedPassword,
+    email: email,
+    emailAuthenticated: false,
+  });
+
+  //check if the user already exits
+
+  let userExists;
+  try {
+    userExists = await UserSchema.findOne({ email });
+  } catch (err) {
+    return res.send({
+      status: 400,
+      message: "Internal server error, Please try again",
+      error: err,
+    });
+  }
+
+  if (userExists) {
+    return res.send({
+      status: 400,
+      message: "User already exists",
+    });
+  }
+
+  //genrate a token
+  const verificationToken = jwtSign(email);
+  console.log(verificationToken);
+  try {
+    const userDB = await user.save(); // create opt in database
+    console.log(userDB);
+    //send verfication mail to user
+    sendVerifcationEmail(email, verificationToken);
+
+    return res.send({
+      status: 200,
+      message:
+        "Verification has been sent to your mail Id. Please verify before login",
+      data: {
+        _id: userDB._id,
+        username: userDB.username,
+        email: userDB.email,
+      },
+    });
+  } catch (err) {
+    return res.send({
+      status: 400,
+      message: "Internal Server Error, Please try again",
+      error: err,
+    });
+  }
+});
+
+app.get("/verifyEmail/:id", (req, res) => {
+  const token = req.params.id;
+  console.log(req.params);
+  jwt.verify(token, "GYANPRAKASHGPS", async (err, verifiedJwt) => {
+    if (err) res.send(err);
+
+    console.log(verifiedJwt);
+
+    const userDb = await UserSchema.findOneAndUpdate(
+      { email: verifiedJwt.email },
+      { emailAuthenticated: true }
+    );
+    console.log(userDb);
+    if (userDb) {
+      return res.status(200).redirect("/login");
+    } else {
+      return res.send({
+        status: 400,
+        message: "Invalid Session link",
+      });
+    }
+  });
+  return res.status(200);
+});
+
+app.post("/login", async (req, res) => {
+  console.log(req.session);
+  const { loginId, password } = req.body;
+
+  if (
+    typeof loginId !== "string" ||
+    typeof password !== "string" ||
+    !loginId ||
+    !password
+  ) {
+    return res.send({
+      status: 400,
+      message: "Invalid Data",
+    });
+  }
+
+
+  let userDB;
+
+  try {
+    if (validator.isEmail(loginId)) {
+      userDB = await UserSchema.findOne({ email: loginId });
+    } else {
+      userDB = await UserSchema.findOne({ username: loginId });
+    }
+
+    console.log(userDB);
+
+    if (!userDB) {
+      return res.send({
+        status: 400,
+        message: "User not found, Please register first",
+        error: err,
+      });
+    }
+
+    // check for email authentication
+    if (userDB.emailAuthenticated === false) {
+      return res.send({
+        status: 400,
+        message: "Please verifiy your mailid",
+      });
+    }
+
+    //comapre the password
+    const isMatch = await bcrypt.compare(password, userDB.password);
+
+    if (!isMatch) {
+      return res.send({
+        status: 400,
+        message: "Invalid Password",
+        data: req.body,
+      });
+    }
+
+    //final return
+    req.session.isAuth = true;
+    req.session.user = {
+      username: userDB.username,
+      email: userDB.email,
+      userId: userDB._id,
+    };
+
+    res.redirect("/dashboard");
+  } catch (err) {
+    return res.send({
+      status: 400,
+      message: "Internal Server Error, Please loggin again!",
+      error: err,
+    });
+  }
+});
+
+app.get("/home", isAuth, (req, res) => {
+  if (req.session.isAuth) {
+    return res.send({
+      message: "This is your home page",
+    });
+  } else {
+    return res.send({
+      message: "Please Logged in again",
+    });
+  }
+});
+
+app.post("/logout", isAuth, (req, res) => {
+  req.session.destroy((err) => {
+    if (err) throw err;
+
+    res.redirect("/login");
+  });
+});
+
+app.post("/logout_from_all_devices", isAuth, async (req, res) => {
+  console.log(req.session.user.username);
+  const username = req.session.user.username;
+
+  const Schema = mongoose.Schema;
+  const sessionSchema = new Schema({ _id: String }, { strict: false });
+  const SesisonModel = mongoose.model("session", sessionSchema);
+
+  try {
+    const sessionDb = await SesisonModel.deleteMany({
+      "session.user.username": username,
+    });
+    console.log(sessionDb);
+    return res.send({
+      status: 200,
+      message: "Logged out from all devices",
+    });
+  } catch (err) {
+    return res.send({
+      status: 400,
+      message: "Logout from all devices failed",
+      error: err,
+    });
+  }
+});
+
+app.get("/dashboard", isAuth, async (req, res) => {
+
+  res.render("dashboard");
+});
+
+app.post("/pagination_dashboard", isAuth, async (req, res) => {
+  const skip = req.query.skip || 0;
+  const LIMIT = 5;
+  const username = req.session.user.username;
+
+  //mongodb aggregation -> is we want to perform multiple actions in db, then we use aggregation
+  try {
+    let todos = await TodoModel.aggregate([
+      { $match: { username: username } },
+      {
+        $facet: {
+          data: [{ $skip: parseInt(skip) }, { $limit: LIMIT }],
+        },
+      },
+    ]);
+
+    return res.send({
+      status: 200,
+      message: "Read Successfully",
+      data: todos,
+    });
+  } catch (err) {
+    return res.send({
+      status: 400,
+      message: "Database error. Please try again",
+      error: err,
+    });
+  }
+});
+
+app.post("/create-item", isAuth, rateLimitng, async (req, res) => {
+  console.log(req.body);
+  const todoText = req.body.todo;
+
+  if (!todoText) {
+    return res.send({
+      status: 400,
+      message: "Missing Parameters",
+    });
+  }
+
+  if (todoText.length > 100) {
+    return res.send({
+      status: 400,
+      message: "Todo text is very long. Max 100 characters allowd.",
+    });
+  }
+
+  let todo = new TodoModel({
+    todo: todoText,
+    username: req.session.user.username,
+  });
+
+  try {
+    const todoDb = await todo.save();
+    return res.send({
+      status: 200,
+      message: "Todo created successfully",
+      data: todoDb,
+    });
+  } catch (err) {
+    return res.send({
+      status: 400,
+      message: "Database error, Please Try again.",
+    });
+  }
+});
+
+app.post("/edit-item", isAuth, async (req, res) => {
+  const id = req.body.id;
+  const newData = req.body.newData;
+  console.log(req.body);
+  if (!id || !newData) {
+    return res.send({
+      status: 404,
+      message: "Missing Paramters.",
+      error: "Missing todo data",
+    });
+  }
+
+  try {
+    const todoDb = await TodoModel.findOneAndUpdate(
+      { _id: id },
+      { todo: newData }
+    );
+    return res.send({
+      status: 200,
+      message: "Updated todo succesfully",
+      data: todoDb,
+    });
+  } catch (err) {
+    return res.send({
+      status: 400,
+      message: "Database error, Please Try again.",
+      error: err,
+    });
+  }
+});
+
+app.post("/delete-item", isAuth, async (req, res) => {
+  const id = req.body.id;
+  console.log(req.body);
+  if (!id) {
+    return res.send({
+      status: 404,
+      message: "Missing parameters",
+      error: "Missing id of todo to delete",
+    });
+  }
+
+  try {
+    const todoDb = await TodoModel.findOneAndDelete({ _id: id });
+
+    return res.send({
+      status: 200,
+      message: "Todo Deleted Succesfully",
+      data: todoDb,
+    });
+  } catch (err) {
+    return res.send({
+      status: 400,
+      message: "Database error. Please try again.",
+      error: err,
+    });
+  }
+});
+
+// Forget Password
+app.post("/forget",async(req,res)=>{
+  console.log(req.query)
+  let email = req.query.email;
+  let password = req.query.password;
+  let confirmPassword = req.query.confirmPassword;
+
+  if(password.length<5 || password!==confirmPassword || !validator.isEmail(email)){
+    return res.send({
+      status:400,
+      message:"Something id wrong "
+    })
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 7);
+
+
+  let userExists;
+  try {
+    userExists = await UserSchema.findOneAndUpdate({ email });
+  } catch (err) {
+    return res.send({
+      status: 400,
+      message: "Internal server error, Please try again",
+      error: err,
+    });
+  }
+  
+  if(!userExists){
+    return res.send({
+      status:400,
+      message:"User does not Exist"
+    })
+  }
+
+  // genrate a token
+  const verificationToken = jwtSign(email);
+  console.log(verificationToken);
+  try {
+    let userDB=await UserSchema.findOneAndUpdate({ email },{"$set":{emailAuthenticated:false,password:hashedPassword}}); // create opt in database
+    
+    //send verfication mail to user
+    sendVerifcationEmail(email, verificationToken);
+
+    return res.send({
+      status: 200,
+      message:
+        "Verification has been sent to your mail Id. Please verify before login",
+      data: {
+        _id: userDB._id,
+        username: userDB.username,
+        email: userDB.email,
+      },
+    });
+  } catch (err) {
+    return res.send({
+      status: 400,
+      message: "Internal Server Error, Please try again",
+      error: err,
+    });
+  }
+
+})
+
+//  Resend Email
+app.post("/resend",async(req,res)=>{
+  let email = req.query.email;
+  if(!validator.isEmail(email)){
+    return res.send({
+      status: 400,
+      message: "Not an Email"
+    });
+  }
+  let userExists;
+  try {
+    userExists = await UserSchema.findOneAndUpdate({ email });
+  } catch (err) {
+    return res.send({
+      status: 400,
+      message: "Internal server error, Please try again",
+      error: err,
+    });
+  }
+  
+  if(!userExists){
+    return res.send({
+      status:400,
+      message:"User does not Exist"
+    })
+  }
+
+  // genrate a token
+  const verificationToken = jwtSign(email);
+  console.log(verificationToken);
+  try {
+    //send verfication mail to user
+    sendVerifcationEmail(email, verificationToken);
+
+    return res.send({
+      status: 200,
+      message:
+        "Verification has been sent to your mail Id. Please verify before login",
+      data: {
+        _id: userDB._id,
+        username: userDB.username,
+        email: userDB.email,
+      },
+    });
+  } catch (err) {
+    return res.send({
+      status: 400,
+      message: "Internal Server Error, Please try again",
+      error: err,
+    });
+  }
+})
+
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Listenning on port ${PORT}`);
+});
+
+//Create an ejs Template for register and loggin
+//hashed Password
+//Create a mongoDB connection
+//User Schema
+
+//lecture 7
+//checkuser before insertion /register
+
+//'/login'
+//validate the data
+//find user using either mail or username
+//for found user matched the password
+//include session info to check futher
+//adding session and on database
+
+//dashboard template
+//'logout' and 'logout from all devices
+//isAuth
+
+//create item, edit item, delete item
+//axios package
+//browser
+//pagination
+//rate limiting
---
+++ public/browser.js
@@ -0,0 +1,178 @@
+const config = {
+  headers: {
+    "content-type": "application/json",
+  },
+};
+
+let skip = 0;
+
+document.addEventListener("click", function (event) {
+  if (event.target.classList.contains("add_item")) {
+    event.preventDefault();
+
+    const todoText = document.getElementById("create_field");
+
+    if (todoText.value === "") {
+      alert("Please enter todo text");
+      return;
+    }
+
+    axios
+      .post(
+        "/create-item",
+        JSON.stringify({
+          todo: todoText.value,
+        }),
+        config
+      )
+      .then((res) => {
+        if (res.data.status !== 200) {
+          alert(res.data.message);
+          return;
+        }
+        todoText.value = "";
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  if (event.target.classList.contains("edit-me")) {
+    const id = event.target.getAttribute("data-id");
+    const newData = prompt("Eneter your new todo text");
+
+    axios
+      .post(
+        "/edit-item",
+        JSON.stringify({
+          id,
+          newData,
+        }),
+        config
+      )
+      .then((res) => {
+        console.log(res);
+        if (res.data.status !== 200) {
+          alert(res.data.message);
+          return;
+        }
+
+        event.target.parentElement.parentElement.querySelector(
+          ".item-text"
+        ).innerHTML = newData;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  if (event.target.classList.contains("delete-me")) {
+    const id = event.target.getAttribute("data-id");
+
+    axios
+      .post(
+        "/delete-item",
+        JSON.stringify({
+          id,
+        }),
+        config
+      )
+      .then((res) => {
+        if (res.data.status !== 200) {
+          alert(res.data.message);
+          return;
+        }
+        event.target.parentElement.parentElement.remove();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  if (event.target.getAttribute("id") === "show_more") {
+    console.log("show more");
+    generateTodos();
+  }
+  
+  if(event.target.classList.contains("forget")){
+    console.log("forget")
+    handleForget();
+  }
+
+  if(event.target.classList.contains("resend")){
+    console.log("resend");
+    handleResend();
+  }
+});
+
+async function handleForget(){
+  let email = prompt("Please Enter your valid Email")
+  let password  = prompt("Please Enter your password");
+  let confirmPassword  = prompt("Please Confirm your password");
+  axios.post(`/forget?email=${email}&password=${password}&confirmPassword=${confirmPassword}`, JSON.stringify({}), config)
+  .then((res)=>{
+    console.log(res)
+    alert(res.message)
+    alert("Verification has been sent to your mail Id. Please verify before login")
+  })
+  .catch((err)=>{
+    console.log(err)
+    alert(err.message)
+  })
+}
+
+function handleResend(){
+  let email = prompt("Please Enter your valid Email")
+  axios.post(`/resend?email=${email}`, JSON.stringify({}), config)
+  .then((res)=>{
+    console.log(res)
+    alert("Verification has been sent to your mail Id. Please verify before login")
+  })
+  .catch((err)=>{
+    console.log(err)
+    alert(err.message)
+  })
+}
+
+window.onload = function () {
+  generateTodos();
+};
+
+function generateTodos() {
+  axios
+    .post(`/pagination_dashboard?skip=${skip}`, JSON.stringify({}), config)
+    .then((res) => {
+      if (res.status !== 200) {
+        alert("Failed to Read, Please try again!");
+        return;
+      }
+      console.log(res.data.data[0].data);
+      const todoList = res.data.data[0].data;
+
+      if (todoList.length === 0) {
+        alert("No more todos to show, please create some");
+        return;
+      }
+
+      document.getElementById("item_list").insertAdjacentHTML(
+        "beforeend",
+        todoList
+          .map((item) => {
+            return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+          <span class="item-text"> ${item.todo} </span>
+          <div>
+          <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+          <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
+      </div>
+      </li>`;
+          })
+          .join("")
+      );
+
+      skip += todoList.length;
+    })
+    .catch((err) => {
+      console.log(err);
+      alert("Something went wrong!");
+    });
+}
+
